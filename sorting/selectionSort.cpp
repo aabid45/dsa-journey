d1835ff2b65290e611166a9217f779e475479cda
aabid45 <mdaabidalam01@gmail.com>
2026-09-12 22:54:53 +0530

    Day17(Sorting): revised selection sort with dry run

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -1,64 +1,23 @@
 // sort in ascending order
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// void selectionSort(int arr[], int n)
-// {
-//     for (int i = 0; i <= n - 2; i++)
-//     {
-//         int minimum = i;
-//         for (int j = i + 1; j <= n; j++)
-//         {
-//             if (arr[j] < arr[minimum])
-//             {
-//                 minimum = j;
-//                 cout << arr[minimum] << endl;
-//             }
-//         }
-//         int tempp = arr[minimum];
-//         arr[minimum] = arr[i];
-//         arr[i] = tempp;
-//     }
-// }
-
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> arr[i];
-//     }
-//     selectionSort(arr, n);
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout << arr[i];
-//     }
-
-//     return 0;
-// }
-
-//****** sort in descending order
 #include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int n, int arr[])
+void selectionSort(int arr[], int n)
 {
     for (int i = 0; i <= n - 2; i++)
     {
-        int maxindex = i;
-        for (int j = i + 1; j <= n-1; j++)
+        int minimum = i;
+        for (int j = i + 1; j <= n; j++)
         {
-            if (arr[j] > arr[maxindex])
+            if (arr[j] < arr[minimum])
             {
-                maxindex = j;
+                minimum = j;
+                cout << arr[minimum] << endl;
             }
         }
-        int temp = arr[maxindex];
-        arr[maxindex] = arr[i];
-        arr[i] = temp;
+        int tempp = arr[minimum];
+        arr[minimum] = arr[i];
+        arr[i] = tempp;
     }
 }
 
@@ -71,10 +30,51 @@ int main()
     {
         cin >> arr[i];
     }
-    selectionSort(n, arr);
+    selectionSort(arr, n);
 
     for (int i = 0; i < n; i++)
     {
         cout << arr[i];
     }
+
+    return 0;
 }
+
+//****** sort in descending order
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void selectionSort(int n, int arr[])
+// {
+//     for (int i = 0; i <= n - 2; i++)
+//     {
+//         int maxindex = i;
+//         for (int j = i + 1; j <= n-1; j++)
+//         {
+//             if (arr[j] > arr[maxindex])
+//             {
+//                 maxindex = j;
+//             }
+//         }
+//         int temp = arr[maxindex];
+//         arr[maxindex] = arr[i];
+//         arr[i] = temp;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     selectionSort(n, arr);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i];
+//     }
+// }
